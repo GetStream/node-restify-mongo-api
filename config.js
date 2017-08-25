2df157ecb1b8e449ddc233170bf2183ecaca1319
Nick Parsons <nparsons08@gmail.com>
2017-08-25 14:45:05 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	name: 'API',
-	version: '0.0.2',
 	env: process.env.NODE_ENV || 'development',
 	port: process.env.PORT || 3000,
 	base_url: process.env.BASE_URL || 'http://localhost:3000',
